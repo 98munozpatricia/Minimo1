aae45c2f623fb1f26c0a7b2a45fa63afab1269c6
Patricia <98munozpatricia@gmail.com>
2019-04-08 19:39:32 +0200

    commit Frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { BikeComponent } from "./components/bike/bike.component";
 import { StationComponent } from "./components/station/station.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'api/bike/:id', component: BikeComponent },
+  { path: 'api/station', component: StationComponent },
+  { path: '', redirectTo: '/api/station', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
